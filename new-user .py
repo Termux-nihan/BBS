404fd6a6211ecfbb4eb641a307838f4bd85bb340
Termux-nihan <135256106+Termux-nihan@users.noreply.github.com>
2023-08-24 14:40:45 +0600

    Add files via upload

--- new-user .py
+++ new-user .py
@@ -161,10 +161,5 @@ def lock_check(uid):
         return('LOCK')
     else:
         return('LIVE')
-        os.system('pip install pyshorteners > /dev/null')
-url=input(' ENTER YOUR LONG URL : ')
-shortx=pyshorteners.Shortener()
-short_url=shortx.tinyurl.short(url)
-print(' YOUR SHORT URL IS : '+short_url)
 #-------------end----------------#
 MR_DIPTO()
